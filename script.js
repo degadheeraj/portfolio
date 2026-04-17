3174a5ee1cd2fdb517553ef543d9342aaea8f8d9
degadheeraj <93273061+degadheeraj@users.noreply.github.com>
2026-04-17 05:18:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-// DARK MODE
-const toggleBtn = document.getElementById("themeToggle");
+// DARK MODE (optional)
+const btn = document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", () => {
+btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-});
-
-// ACTIVE NAV
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav a");
-
-window.addEventListener("scroll", () => {
-  let current = "";
-
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 100;
-    if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach(link => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === "#" + current) {
-      link.classList.add("active");
-    }
-  });
 });
