7d30f1cc318be880fa4ca4a066bf498c5483fa5c
degadheeraj <93273061+degadheeraj@users.noreply.github.com>
2026-04-17 05:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// smooth scroll feel (Ashik style minimal)
+// smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
     e.preventDefault();
